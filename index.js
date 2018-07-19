6c8b45ec715e18a1e2b2670006c6535665162980
Alessandro <a.romanino@gmail.com>
2018-07-19 16:30:34 +0200

    Header: 'content-type', 'application/javascript'
     - add res.setHeader('content-type', 'application/javascript');

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var responseinterceptor = require('responseinterceptor');
 
-module.exports=responseinterceptor.intercept(function(body, bodyContentType ,request, callback){
-    var NewResponse=body;
-    NewResponse=NewResponse.replace('<script type=\"text/javascript\">','');
-    NewResponse=NewResponse.replace('</script>','');
-    callback(NewResponse); // callback function with the new content
-});
+module.exports=function(req,res,next) {
+    res.setHeader('content-type', 'application/javascript');
+    responseinterceptor.interceptOnFly(req,res,function (body, bodyContentType, request, callback) {
+        var NewResponse = body;
+        NewResponse = NewResponse.replace('<script type=\"text/javascript\">', '');
+        NewResponse = NewResponse.replace('</script>', '');
+        callback(NewResponse); // callback function with the new content
+    });
+    next();
+}
